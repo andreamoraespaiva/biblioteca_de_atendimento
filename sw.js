3b86d2e4800b76fb9a6b7ede6c26aa094ae2c576
andreamoraespaiva <andreamoraespaiva@gmail.com>
2026-03-16 01:42:46 -0300

    correção syntax error + cache v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'orayon-suporte-v1';
+const CACHE = 'orayon-suporte-v3';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
